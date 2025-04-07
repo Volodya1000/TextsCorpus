2da80da55b1c30b54e150edf20e8217464b01b13
volodya.dichkovski <volodya.dichkovski@gmail.com>
2025-04-07 20:33:45 +0300

    Добавьте файлы проекта.

---
+++ TextsCorpus.WebApi/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+var configuration = builder.Configuration;
+
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
+
